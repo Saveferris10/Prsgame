01110389cedbcd9bff6fe528d65cc9efce519d23
Faress Berhe <177858383+Saveferris10@users.noreply.github.com>
2024-11-05 13:28:24 -0500

    Added func for human choice and computer choice

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+    let compNumb = Math.floor(Math.random() * 3);
+
+    if (compNumb == 0) {
+        console.log('Computer choice is Paper');
+    }
+        else if (compNumb == 1) {
+            console.log('Computer choice is Rock');
+        }
+        else {
+            console.log('Computer choice is Scissors');
+        }
+
+}
+getComputerChoice();
+
+function getHumanChoice () {
+    let HumNumb = console.log(prompt('Choose Paper, Rock, or Scissors?'));
+
+   // if (HumNumb === 'Paper') 
+}
+getHumanChoice();
