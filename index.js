bc42e24e08ea2c0a03df789070b05b6f8149f000
Faress Berhe <177858383+Saveferris10@users.noreply.github.com>
2024-11-07 10:08:08 -0500

    function to get user and computer choice. func to set rules of the game

--- index.js
+++ index.js
@@ -12,13 +12,40 @@ function getComputerChoice() {
         else {
             console.log('Computer choice is Scissors');
         }
-
+    return compNumb;
 }
 getComputerChoice();
 
 function getHumanChoice () {
     let HumNumb = console.log(prompt('Choose Paper, Rock, or Scissors?'));
+    return HumNumb;
 
    // if (HumNumb === 'Paper') 
+
 }
 getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(HumNumb, compNumb) {
+    if (compNumb === 'Paper' && HumNumb === 'Rock') {
+        console.log('You Lose! Paper beats Rock');
+    }
+    else if (compNumb === 'Paper' && HumNumb === 'Scissors') {
+        console.log('You Win! Scissors beats Paper');
+    }
+    else if (compNumb === 'Rock' && HumNumb === 'Scissors') {
+        console.log('You Lose! Rock beats Scissors');
+    }
+    else if (compNumb === 'Rock' && HumNumb === 'Paper') {
+        console.log('You Win! Paper beats Rock');
+    }
+    else if (compNumb === 'Scissors' && HumNumb === 'Rock') {
+        console.log('You Win! Rock beats Scissors');
+    }
+    else if (compNumb === 'Scissors' && HumNumb === 'Paper') {
+        console.log('You Lose! Scissors beats Paper');
+    }
+}
+playRound();
