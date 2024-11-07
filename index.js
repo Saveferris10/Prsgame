5640e289613faeb4e6577e81b19bcdd79584c72a
Faress Berhe <177858383+Saveferris10@users.noreply.github.com>
2024-11-07 14:21:03 -0500

    rules to game function updated

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-console.log("Hello World");
+let compNumb = Math.floor(Math.random() * 3);
 
 function getComputerChoice() {
-    let compNumb = Math.floor(Math.random() * 3);
-
     if (compNumb == 0) {
         console.log('Computer choice is Paper');
+        return compNumb = 'Paper'
     }
         else if (compNumb == 1) {
             console.log('Computer choice is Rock');
+        return compNumb = 'Rock'
         }
         else {
             console.log('Computer choice is Scissors');
+        return compNumb = 'Scissors'
         }
-    return compNumb;
 }
 getComputerChoice();
 
-function getHumanChoice () {
-    let HumNumb = console.log(prompt('Choose Paper, Rock, or Scissors?'));
-    return HumNumb;
+//function getHumanChoice () {
+    let HumNumb = prompt('Choose Paper, Rock, or Scissors?');
+    console.log(HumNumb);
+//}
+//getHumanChoice();
 
-   // if (HumNumb === 'Paper') 
-
-}
-getHumanChoice();
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(HumNumb, compNumb) {
-    if (compNumb === 'Paper' && HumNumb === 'Rock') {
+    if (compNumb == 'Paper' && HumNumb == 'Rock') {
         console.log('You Lose! Paper beats Rock');
     }
-    else if (compNumb === 'Paper' && HumNumb === 'Scissors') {
+    else if (compNumb == 'Paper' && HumNumb == 'Scissors') {
         console.log('You Win! Scissors beats Paper');
     }
-    else if (compNumb === 'Rock' && HumNumb === 'Scissors') {
+    else if (compNumb == 'Rock' && HumNumb == 'Scissors') {
         console.log('You Lose! Rock beats Scissors');
     }
-    else if (compNumb === 'Rock' && HumNumb === 'Paper') {
+    else if (compNumb == 'Rock' && HumNumb == 'Paper') {
         console.log('You Win! Paper beats Rock');
     }
-    else if (compNumb === 'Scissors' && HumNumb === 'Rock') {
+    else if (compNumb == 'Scissors' && HumNumb == 'Rock') {
         console.log('You Win! Rock beats Scissors');
     }
-    else if (compNumb === 'Scissors' && HumNumb === 'Paper') {
-        console.log('You Lose! Scissors beats Paper');
+    else if (compNumb == 'Scissors' && HumNumb == 'Paper') {
+        console.log('You Lose! Scissors beats Paper');}
+    else {
+        console.log('Tie game! Try again');
     }
-}
-playRound();
+    }
+playRound(HumNumb, compNumb);
