4a69101241e4e33dfd5d934cf48b45321dfbccba
Faress Berhe <177858383+Saveferris10@users.noreply.github.com>
2024-11-12 12:02:59 -0500

    added func for score per round

--- index.js
+++ index.js
@@ -30,31 +30,45 @@ function playRound(HumNumb, compNumb) {
     if (compNumb == 'Paper' && HumNumb == 'Rock') {
         console.log('You Lose! Paper beats Rock');
         computerScore++;
+        return computerScore;
     }
     else if (compNumb == 'Paper' && HumNumb == 'Scissors') {
         console.log('You Win! Scissors beats Paper');
         humanScore++;
+        return humanScore;
     }
     else if (compNumb == 'Rock' && HumNumb == 'Scissors') {
         console.log('You Lose! Rock beats Scissors');
         computerScore++
+        return computerScore;
     }
     else if (compNumb == 'Rock' && HumNumb == 'Paper') {
         console.log('You Win! Paper beats Rock');
         humanScore++;
+        return humanScore;
     }
     else if (compNumb == 'Scissors' && HumNumb == 'Rock') {
         console.log('You Win! Rock beats Scissors');
         humanScore++;
+        return humanScore;
     }
     else if (compNumb == 'Scissors' && HumNumb == 'Paper') {
         console.log('You Lose! Scissors beats Paper');
         computerScore++;
+        return computerScore;
     }
     else {
         console.log('Tie game! Try again');
     }
     }
 playRound(HumNumb, compNumb);
+
+function playGame() {
+    humanScore;
+    computerScore;
+    for (i = 0; i <= 5; i++);
+    return playRound;
+};
+playGame(playRound);
 console.log('Your score is ' + humanScore);
 console.log('The computer score is ' + computerScore);
